e007d6c6c7967f5787d30a638078dcd7026e4109
Cynthia London <cynthe17@gmail.com>
2017-11-08 16:38:42 -0600

    math.random

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,3 +3,11 @@ var letter = ["a", "b", "c", "d", "e", "f", "g",
     "o", "p", "q", "r", "s", "t", "u",
     "v", "w", "x", "y", "z"
 ];
+
+var wins = 0;
+var losses = 0;
+var ties = 0;
+var yourGuesses = 10;
+
+var computerchoice = letter(Math.random) * letter.length)];
+console log(computerchoice)
