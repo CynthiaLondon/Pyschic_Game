d488ac401cd3d0fb426d7a6c78f52275cbbaba37
Cynthia London <cynthe17@gmail.com>
2017-11-10 16:51:42 -0600

    javascript vars

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,2 +1,10 @@
 var alphabetLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", 
 "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+var wins = 0;
+var losses = 0;
+var leftGuesses = 10;
+var guessesMade = [];
+var userGuess = null;
+var letterGuess = alphabetLetters[Math.floor(Math.random() * alphabetLetters.length)];
+console.log("Wins: " + wins + " Losses: " + losses + " leftGuesses: " + leftGuesses + " Guesses Made: " + guessesMade + " Computer picked: " + letterGuess);
